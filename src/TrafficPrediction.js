869a5352641fb6e2df3159856e266aad1e7f5c91
bkazic <blaz.kazic@ijs.si>
2014-09-08 08:34:38 +0200

    Add tic and toc. Add singular vector decomposition (SVD). Add visualisation Changed analytics (on resampledStore) trigger, from .addTrigger() to .addStreamAggr() Changed reporting frequency: from every rec, to 1 per day.

--- src/TrafficPrediction.js
+++ src/TrafficPrediction.js
@@ -1,5 +1,12 @@
 var analytics = require('analytics.js');
 var evaluation = require('evaluation.js')
+var tm = require('time')
+var utilities = require('utilities.js');
+viz = require('visualization.js');
+
+// Create instance for stop watch
+sw = new utilities.clsStopwatch();
+sw2 = new utilities.clsStopwatch();
 
 // Import modules from lib
 var Service = {}; Service.Mobis = {}; Service.Mobis.Utils = {};
@@ -21,6 +28,7 @@ var avrValMAE = Service.Mobis.Utils.Stat.newMeanAbsoluteError();
 var prevValMAE = Service.Mobis.Utils.Stat.newMeanAbsoluteError();
 var linregMAE = Service.Mobis.Utils.Stat.newMeanAbsoluteError();
 var ridgeRegMAE = Service.Mobis.Utils.Stat.newMeanAbsoluteError();
+var ridgeRegSVDMAE = Service.Mobis.Utils.Stat.newMeanAbsoluteError();
 var svmrMAE = Service.Mobis.Utils.Stat.newMeanAbsoluteError();
 var nnMAE = Service.Mobis.Utils.Stat.newMeanAbsoluteError();
 var knnMAE = Service.Mobis.Utils.Stat.newMeanAbsoluteError();
@@ -192,75 +200,90 @@ resampledStore.addStreamAggr({
     emaType: "previous", interval: 120 * 60 * 1000, initWindow: 10 * 60 * 1000
 });
 
-// Buffer defines for how many records infront prediction will be learned
+// Buffer defines for how many records infront prediction will be learned (2 is one step ahaed)
 resampledStore.addStreamAggr({ name: "delay", type: "recordBuffer", size: 7 });
 
 ////////////////////////////// DEFINING FEATURE SPACE //////////////////////////////
 // define features
 var features = [
-    { type: "constant", source: resampledStore.name, val: 1 },
-    //{ type: "jsfunc", source: resampledStore.name, name: "slovanianHolidays", dim: 1, fun: slovenianHolidayFtr.getFtr },
-    //{ type: "jsfunc", source: resampledStore.name, name: "foolMoon", dim: 1, fun: fullMoonFtr.getFtr },
+    //{ type: "constant", source: resampledStore.name, val: 1 },
+    { type: "jsfunc", source: resampledStore.name, name: "slovanianHolidays", dim: 1, fun: slovenianHolidayFtr.getFtr },
+    { type: "jsfunc", source: resampledStore.name, name: "foolMoon", dim: 1, fun: fullMoonFtr.getFtr },
     //{ type: "jsfunc", source: resampledStore.name, name: "weekend", dim: 1, fun: weekendFtr.getFtr },
     { type: "jsfunc", source: resampledStore.name, name: "weekend", dim: 1, fun: function (rec) {return (rec.DateTime.dayOfWeekNum == 0 || rec.DateTime.dayOfWeekNum == 6) ? 1 : 0 } },
     { type: "jsfunc", source: resampledStore.name, name: "historicalValues", dim: histVals.getSize(), fun: histVals.getVals },
 
-    { type: "numeric", source: resampledStore.name, field: "Speed", normalize: false },
-    //{ type: "numeric", source: resampledStore.name, field: "Ema1", normalize: false },
-    //{ type: "numeric", source: resampledStore.name, field: "Ema2", normalize: false },
-    //{ type: "numeric", source: resampledStore.name, field: "NumOfCars", normalize: false },
-    //{ type: "numeric", source: resampledStore.name, field: "Gap", normalize: false },
-    //{ type: "numeric", source: resampledStore.name, field: "Occupancy", normalize: false },
-    //{ type: "numeric", source: resampledStore.name, field: "TrafficStatus", normalize: false },
+    //{ type: "numeric", source: resampledStore.name, field: "Speed", normalize: false },
+    { type: "numeric", source: resampledStore.name, field: "Ema1", normalize: false },
+    { type: "numeric", source: resampledStore.name, field: "Ema2", normalize: false },
+    { type: "numeric", source: resampledStore.name, field: "NumOfCars", normalize: false },
+    { type: "numeric", source: resampledStore.name, field: "Gap", normalize: false },
+    { type: "numeric", source: resampledStore.name, field: "Occupancy", normalize: false },
+    { type: "numeric", source: resampledStore.name, field: "TrafficStatus", normalize: false },
 
-    //{ type: "numeric", source: resampledStore.name, field: "temperature", normalize: false },
-    //{ type: "numeric", source: resampledStore.name, field: "visibility", normalize: false },
-    //{ type: "numeric", source: resampledStore.name, field: "clearDay", normalize: false },
-    //{ type: "numeric", source: resampledStore.name, field: "clearNight", normalize: false },
-    //{ type: "numeric", source: resampledStore.name, field: "rain", normalize: false },
-    //{ type: "numeric", source: resampledStore.name, field: "snow", normalize: false },
-    //{ type: "numeric", source: resampledStore.name, field: "sleet", normalize: false },
-    //{ type: "numeric", source: resampledStore.name, field: "wind", normalize: false },
-    //{ type: "numeric", source: resampledStore.name, field: "fog", normalize: false },
-    //{ type: "numeric", source: resampledStore.name, field: "cloudy", normalize: false },
-    //{ type: "numeric", source: resampledStore.name, field: "partlyCloudyDay", normalize: false },
-    //{ type: "numeric", source: resampledStore.name, field: "parltlyCloudyNight", normalize: false },
+    { type: "numeric", source: resampledStore.name, field: "temperature", normalize: false },
+    { type: "numeric", source: resampledStore.name, field: "visibility", normalize: false },
+    { type: "numeric", source: resampledStore.name, field: "clearDay", normalize: false },
+    { type: "numeric", source: resampledStore.name, field: "clearNight", normalize: false },
+    { type: "numeric", source: resampledStore.name, field: "rain", normalize: false },
+    { type: "numeric", source: resampledStore.name, field: "snow", normalize: false },
+    { type: "numeric", source: resampledStore.name, field: "sleet", normalize: false },
+    { type: "numeric", source: resampledStore.name, field: "wind", normalize: false },
+    { type: "numeric", source: resampledStore.name, field: "fog", normalize: false },
+    { type: "numeric", source: resampledStore.name, field: "cloudy", normalize: false },
+    { type: "numeric", source: resampledStore.name, field: "partlyCloudyDay", normalize: false },
+    { type: "numeric", source: resampledStore.name, field: "parltlyCloudyNight", normalize: false },
 
-    //{ type: "numeric", source: resampledStore.name, field: "Priority", normalize: false },
-    //{ type: "numeric", source: resampledStore.name, field: "RoadPriorityFlt", normalize: false },
-    //{ type: "numeric", source: resampledStore.name, field: "TrafficJam", normalize: false },
-    //{ type: "numeric", source: resampledStore.name, field: "RoadClosure", normalize: false },
-    //{ type: "numeric", source: resampledStore.name, field: "TrafficAccident", normalize: false },
-    //{ type: "numeric", source: resampledStore.name, field: "OtherEvents", normalize: false },
-    //{ type: "numeric", source: resampledStore.name, field: "Ice", normalize: false },
-    //{ type: "numeric", source: resampledStore.name, field: "Wind", normalize: false },
-    //{ type: "numeric", source: resampledStore.name, field: "NoFreightTraffic", normalize: false },
-    //{ type: "numeric", source: resampledStore.name, field: "SnowEvent", normalize: false },
+    { type: "numeric", source: resampledStore.name, field: "Priority", normalize: false },
+    { type: "numeric", source: resampledStore.name, field: "RoadPriorityFlt", normalize: false },
+    { type: "numeric", source: resampledStore.name, field: "TrafficJam", normalize: false },
+    { type: "numeric", source: resampledStore.name, field: "RoadClosure", normalize: false },
+    { type: "numeric", source: resampledStore.name, field: "TrafficAccident", normalize: false },
+    { type: "numeric", source: resampledStore.name, field: "OtherEvents", normalize: false },
+    { type: "numeric", source: resampledStore.name, field: "Ice", normalize: false },
+    { type: "numeric", source: resampledStore.name, field: "Wind", normalize: false },
+    { type: "numeric", source: resampledStore.name, field: "NoFreightTraffic", normalize: false },
+    { type: "numeric", source: resampledStore.name, field: "SnowEvent", normalize: false },
 
-    //{ type: "multinomial", source: resampledStore.name, field: "DateTime", datetime: true }
+    { type: "multinomial", source: resampledStore.name, field: "DateTime", datetime: true }
 ];
 // Feature extractors for feature space
 var ftrSpace = analytics.newFeatureSpace(features);
 
+// load truncated svd decomposition matrix U
+//var fin = fs.openRead("svd23")
+var fin = fs.openRead("svd70")
+//var fin = fs.openRead("svd91")
+//var U = la.newSpMat().load(fin).full();
+var U = la.newMat().load(fin);
 
 ///////////////// INITIALIZING ANALYTIC ALGORITHMS FOR PREDICTION //////////////////
 // Initialize analytics
 //var avr = Service.Mobis.Utils.Baseline.newAvrVal();
 var linreg = analytics.newRecLinReg({ "dim": ftrSpace.dim, "forgetFact": 0.98, "regFact": 10000 });
-var ridgeRegression = new analytics.ridgeRegression(10000, ftrSpace.dim);
+var ridgeRegression = new analytics.ridgeRegression(10000, ftrSpace.dim, 100);
 //var svmr = Service.Mobis.Utils.OnlineSVMR.newSvmRegression(ftrSpace.dim, { "c": 0.01, "eps": 0.00001, "maxTime": 0.1, "maxIterations": 100000, batchSize: 100}, 10);
 var NN = analytics.newNN({ "layout": [ftrSpace.dim, 4, 1], "tFuncHidden": "sigmoid", "tFuncOut": "linear", "learnRate": 0.2, "momentum": 0.2 });
 var knn = analytics.newKNearestNeighbors(3, 100);
+var ridgeRegressionSVD = new analytics.ridgeRegression(1, U.cols, 100);
+
+sw.tic();
+sw2.tic();
+var prevRecDay = null;
 
 //////////////////////////// PREDICTION AND EVALUATION ////////////////////////////
-resampledStore.addTrigger({
+resampledStore.addStreamAggr({
+    name: "analytics",
     onAdd: function (rec) {
         // Adds ema-s to rec
         rec.Ema1 = resampledStore.getStreamAggr("Ema1").val.Val;
         rec.Ema2 = resampledStore.getStreamAggr("Ema2").val.Val;
 
-        console.log("\nRec for time: " + rec.DateTime.string);
-        ftrSpace.ftrVec(rec).print()
+        //console.log("Tu sn...")
+        //console.pause();
+        //eval(breakpoint);
+        //console.log("\nRec for time: " + rec.DateTime.string);
+        //ftrSpace.ftrVec(rec).print()
         //console.startx(function (x) { return eval(x); })
         //console.log("X cols for SVMR: " + knn.X.cols)
         //knn.X.print()
@@ -268,7 +291,7 @@ resampledStore.addTrigger({
 
         histVals.update(rec.Speed);
         // Predict and add to rec
-        rec.SpeedLimit = 50;
+        rec.SpeedLimit = trafficStore.last.measuredBy.MaxSpeed;
         rec.PrevValPred = rec.Speed;
         rec.AvrValPred = avr.getAvr();
         rec.LinregPred = linreg.predict(ftrSpace.ftrVec(rec));
@@ -276,6 +299,7 @@ resampledStore.addTrigger({
         //rec.SvmrPred = svmr.predict(ftrSpace.ftrVec(rec));
         rec.NNPred = NN.predict(ftrSpace.ftrVec(rec)).at(0);
         rec.KNNPred = knn.predict(ftrSpace.ftrVec(rec));
+        rec.RidgeRegSVDPred = ridgeRegressionSVD.predict(U.multiplyT(ftrSpace.ftrVec(rec)))
 
         // Get rec for training
         trainRecId = resampledStore.getStreamAggr("delay").val.first;
@@ -291,19 +315,10 @@ resampledStore.addTrigger({
             NN.learn(ftrSpace.ftrVec(resampledStore[trainRecId]), la.newVec([rec.Speed]));
             avr.update(resampledStore[trainRecId].Speed);
             knn.update(ftrSpace.ftrVec(resampledStore[trainRecId]), rec.Speed);
-
-            console.log("Speed:" + rec.Speed);
-            console.log("SpeedLimit: " + 50.0);
-            console.log("AvrVal: " + resampledStore[trainRecId].AvrValPred);
-            console.log("PrevVal: " + resampledStore[trainRecId].PrevValPred);
-            console.log("LinReg: " + resampledStore[trainRecId].LinregPred);
-            console.log("RidgeReg: " + resampledStore[trainRecId].RidgeRegPred);
-            console.log("Svmr: " + resampledStore[trainRecId].SvmrPred);
-            console.log("NN: " + resampledStore[trainRecId].NNPred);
-            console.log("KNN: " + resampledStore[trainRecId].KNNPred + "\n");
+            ridgeRegressionSVD.addupdate(U.multiplyT(ftrSpace.ftrVec(resampledStore[trainRecId])), rec.Speed);
 
             // skip first few iterations because the error of svmr is to high
-            if (rec.$id > 30) {
+            if (rec.$id > 50) {
                 // Calculate mean
                 speedLimitMAE.update(rec.Speed - resampledStore[trainRecId].SpeedLimit)
                 avrValMAE.update(rec.Speed - resampledStore[trainRecId].AvrValPred);
@@ -313,6 +328,7 @@ resampledStore.addTrigger({
                 svmrMAE.update(rec.Speed - resampledStore[trainRecId].SvmrPred);
                 nnMAE.update(rec.Speed - resampledStore[trainRecId].NNPred);
                 knnMAE.update(rec.Speed - resampledStore[trainRecId].KNNPred);
+                ridgeRegSVDMAE.update(rec.Speed - resampledStore[trainRecId].RidgeRegSVDPred);
             }
         }
         // Write errors to store
@@ -324,19 +340,45 @@ resampledStore.addTrigger({
         rec.SvmrPredMAE = svmrMAE.getError();
         rec.NNPredMAE = nnMAE.getError();
         rec.KNNPredMAE = knnMAE.getError();
+        rec.RidgeRegSVDPredMAE = ridgeRegSVDMAE.getError();
 
-        // Write errors to console
-        console.log("Working with rec: " + rec.DateTime.string);
-        console.log("SpeedLimit MAE Error: " + speedLimitMAE.getError());
-        console.log("AvrVal MAE Error: " + avrValMAE.getError());
-        console.log("PrevVal MAE Error: " + prevValMAE.getError());
-        console.log("LinReg MAE Error: " + linregMAE.getError());
-        console.log("RidgeReg MAE Error: " + ridgeRegMAE.getError());
-        console.log("Svmr MAE Error: " + svmrMAE.getError());
-        console.log("NN MAE Error: " + nnMAE.getError());
-        console.log("KNN MAE Error: " + knnMAE.getError() + "\n");
+        // Reporting results
+        // Only one report per day
+        if (rec.DateTime.day != prevRecDay) {
+            sw2.toc("Leap time");
+            sw2.tic();
+            ftrSpace.ftrVec(rec).print()
+            console.log("Working on rec: " + rec.DateTime.dateString+ "\n");
 
-    }
+            // Write predictions to console
+            console.log("Speed:" + rec.Speed);
+            console.log("SpeedLimit: " + rec.SpeedLimit);
+            console.log("AvrVal: " + resampledStore[trainRecId].AvrValPred);
+            console.log("PrevVal: " + resampledStore[trainRecId].PrevValPred);
+            console.log("LinReg: " + resampledStore[trainRecId].LinregPred);
+            console.log("RidgeReg: " + resampledStore[trainRecId].RidgeRegPred);
+            console.log("RidgeRegSVD: " + resampledStore[trainRecId].RidgeRegSVDPred + "\n");
+            console.log("Svmr: " + resampledStore[trainRecId].SvmrPred);
+            console.log("NN: " + resampledStore[trainRecId].NNPred);
+            console.log("KNN: " + resampledStore[trainRecId].KNNPred + "\n");
+
+            // Write errors to console
+            console.log("Working with rec: " + rec.DateTime.string);
+            console.log("SpeedLimit MAE Error: " + speedLimitMAE.getError());
+            console.log("AvrVal MAE Error: " + avrValMAE.getError());
+            console.log("PrevVal MAE Error: " + prevValMAE.getError());
+            console.log("LinReg MAE Error: " + linregMAE.getError());
+            console.log("RidgeReg MAE Error: " + ridgeRegMAE.getError());
+            console.log("RidgeRegSVD MAE Error: " + ridgeRegSVDMAE.getError());
+            console.log("Svmr MAE Error: " + svmrMAE.getError());
+            console.log("NN MAE Error: " + nnMAE.getError());
+            console.log("KNN MAE Error: " + knnMAE.getError() + "\n");
+
+            // set new prevRecDay
+            prevRecDay = rec.DateTime.day;
+        }
+    },
+    saveJson: function () { }
 });
 
 //// Load Stores from log files
@@ -345,11 +387,21 @@ Service.Mobis.Utils.Stores.loadStores();
 // Imports data from loadstores according to timestamp
 var loadStores = [trafficLoadStore, weatherLoadStore, eventLogsLoadStore];
 var targetStores = [trafficStore, weatherStore, eventLogsStore];
-Service.Mobis.Utils.Data.importData(loadStores, targetStores, 1100);
+//Service.Mobis.Utils.Data.importData(loadStores, targetStores, 10000);
+Service.Mobis.Utils.Data.importData(loadStores, targetStores);
 
 // DEBUGGING
 console.start()
 
+var fields = [resampledStore.field("DateTime"),
+              //resampledStore.field("NumOfCars"),
+              resampledStore.field("Speed"),
+              //resampledStore.field("temperature"),
+              //resampledStore.field("visibility"),
+              //resampledStore.field("priority")
+]
+
+viz.drawHighChartsTimeSeries(viz.highchartsConverter(fields, resampledStore.recs.toJSON()), "test.html", { title: { text: "Testing testing.." }, chart: { type: 'spline', zoomType: 'x' }, });
 
 //////////////////////////// ONLINE (REST) SERVICES ////////////////////////////
 // Query records
